34ba9578776550806a514e60fd209d191cb5e956
Eva Szatmari <szateva@gmail.com>
2021-05-27 22:23:47 +0100

    Ex8 solved

---
+++ Ex8_WidgetsAndGizmos.py
@@ -0,0 +1,15 @@
+""" two products: widgets and gizmos
+ Each widget weighs 75 g each
+ Each gizmos weighs 112 g each
+ read from user the no of widgets and gizmos
+ compute and display the total weight"""
+
+widgets = int(input("How many widgets? "))
+gizmos = int(input("How many gizmos? "))
+
+widget_weight = 75
+gizmo_weight = 112
+
+total_weight = widgets * widget_weight + gizmos * gizmo_weight
+
+print("The total weight is: %d g" % total_weight)
