c0a8d6f048d1fb997f5ace8f180628629ff63ea1
Eva Szatmari <szateva@gmail.com>
2021-11-02 21:05:35 +0000

    Ex18 working (formula for volume fixed)

--- Ex18_VolumeOfCylinder.py
+++ Ex18_VolumeOfCylinder.py
@@ -5,7 +5,7 @@ Round result to one dp. """
 radius = float(input("What is the radius of the cylinder? "))
 height = float(input("What is the height of the cylinder? "))
 pi = 3.14  # setting the value of pi
-volume = 2*pi*radius*height
+volume = pi*(radius**2)*height
 volume_2dp = round(volume, 1)
 
 print("The volume of the cylinder rounded to two decimal places is: ", volume_2dp)
