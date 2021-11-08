cba89ea1094eefeca15ba8584df95e9bacaaceac
Eva Szatmari <szateva@gmail.com>
2021-11-08 22:47:31 +0000

    Ex20 final, notes added for ease of reading

--- Ex20 - IdealGasLaw.py
+++ Ex20 - IdealGasLaw.py
@@ -1,8 +1,8 @@
 """ Compute the amount of gas in moles when the user supply:
- pressure (P measured in Pascal)
- volume (V measured in in litres)
- temperature (T measured in Celsius)
- use the formula PV = nRT where R = 8.314 J/(mol K)
+ pressure (P measured in Pascal) float
+ volume (V measured in in litres) float
+ temperature (T measured in Celsius) float
+ use the formula PV = nRT where R = 8.314 J/(mol K) given
  (rearrange to express n, the amount of substance in mol)
  this formula needs temperature in Kelvin (add 273.15 to Celsius)
  Use the following test scenario:
@@ -11,13 +11,13 @@
  T = 20 C """
 
 temperature_C = float(input("What is the temperature of the gas in Celsius? "))
-temperature_change_constant = 273.15
+temperature_change_constant = 273.15    # used to convert Celsius to Kelvin
 temperature_K = temperature_C + temperature_change_constant
-R = 8.314 # ideal gas constant
+R = 8.314 # ideal gas constant, given by the exercise
 pressure = float(input("What is the pressure of the gas in Pascal? "))
 volume = float(input("What is the volume of the gas in litres? "))
-n_mol = (pressure*volume)/(R*temperature_K)
-n_mol_rounded = round(n_mol, 2)
+n_mol = (pressure*volume)/(R*temperature_K) # formula from exercise rearranged to express the number of mol
+n_mol_rounded = round(n_mol, 2) # rounded calculation for ease of reading
 
 print("The amount of gas is ", n_mol_rounded, "mol.")
 
