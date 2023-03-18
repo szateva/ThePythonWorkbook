a636b33ae940e916c2549d119ba2267269946e2c
Eva Szatmari <szateva@gmail.com>
2023-03-18 18:20:20 +0000

    Ex49 is solved. I've checked online and changed the smallest Richer scale to 0.0 to less than 2.0, so the else branch will only be true for 10.0+ magnitude.

--- Ex49 - Richter Scale.py
+++ Ex49 - Richter Scale.py
@@ -18,23 +18,23 @@ considered to be a moderate earthquake.
 
 magnitude = float(input("Please enter the magnitude of the earthquake: "))
 
-if 0 <= magnitude < 2:
+if 0 <= magnitude < 2.0:
     descriptor = "Micro"
-elif 2 <= magnitude < 3:
+elif 2.0 <= magnitude < 3.0:
     descriptor = "Very minor"
-elif 2 <= magnitude < 3:
-    descriptor = "Very minor"
-elif 2 <= magnitude < 3:
-    descriptor = "Very minor"
-elif 2 <= magnitude < 3:
-    descriptor = "Very minor"
-elif 2 <= magnitude < 3:
-    descriptor = "Very minor"
-elif 2 <= magnitude < 3:
-    descriptor = "Very minor"
-elif 2 <= magnitude < 3:
-    descriptor = "Very minor"
-elif 2 <= magnitude < 3:
-    descriptor = "Very minor"
-elif 2 <= magnitude < 3:
-    descriptor = "Very minor"
+elif 3.0 <= magnitude < 4.0:
+    descriptor = "Minor"
+elif 4.0 <= magnitude < 5.0:
+    descriptor = "Light"
+elif 5.0 <= magnitude < 6.0:
+    descriptor = "Moderate"
+elif 6.0 <= magnitude < 7.0:
+    descriptor = "Strong"
+elif 7.0 <= magnitude < 8.0:
+    descriptor = "Major"
+elif 8.0 <= magnitude < 10.0:
+    descriptor = "Great"
+else:
+    descriptor = "Meteoric"
+
+print(f"A magnitude {magnitude} earthquake has the descriptor {descriptor}.")
