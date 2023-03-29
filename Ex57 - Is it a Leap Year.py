b40fb756919256a5090498386e97a7db2dd2b1c7
Eva Szatmari <szateva@gmail.com>
2023-03-29 20:48:27 +0100

    Ex57 solved. I have used some online leap year calculators to check my code's answers against and it passed around 8-10 tests.

---
+++ Ex57 - Is it a Leap Year.py
@@ -0,0 +1,29 @@
+"""
+Most years have 365 days. However, the rime required for the Earth to orbit the Sun is actually slightly more than that.
+As a result, an extra day, February 29, is included in some years to correct for this difference. Such years are referred
+to as leap years. The rules for determining whether a year is a leap year follow:
+
+Any year that is divisible by 400 is a leap year.
+Of the remaining years, any year that is divisible by 100 is NOT a leap year.
+Of the remaining years, any year that is divisible by 4 is a leap year.
+All other years are NOT leap years.
+
+Write a program that reads a year from the user and displays a message indicating whether a year is a leap year.
+"""
+
+year = int(input("Please, enter a year: "))
+
+if (year % 400) == 0:
+    leap = True
+else:
+    if (year % 100) == 0:
+        leap = False
+    else:
+        if (year % 4) == 0:
+            leap = True
+        else:
+            leap = False
+if leap == True:
+    print(f" {year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
