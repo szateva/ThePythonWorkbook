538d95530e1ceda3500912090fd3f067bb0cee3a
Eva Szatmari <szateva@gmail.com>
2023-04-17 14:28:32 +0100

    Ex63, found a mistake in the code, it wasn't printing the correct Farenheit for each row, as the farenheit was outside the loop and was never updated :(

--- Ex63 - Tabulate version.py
+++ Ex63 - Tabulate version.py
@@ -1,9 +1,17 @@
+""" Redoing the previous exercise using tabulate module, following this YouTube video:
+https://www.youtube.com/watch?v=Smf68icE_as"""
+
 """Write a program that displays a conversion table from degrees Celsius to degrees Fahrenheit. The table should include
 rows for all temperatures between 0 and 100 degrees Celsius that are multiples of 10 degrees Celsius. Include appropriate
 headings on your columns. The formula for converting between degrees Celsius and degrees Fahrenheit can be found on the
 internet."""
 
-print("Celsius\tFarenheit")
+from tabulate import tabulate
+
+celsius = 0
+farenheit = (celsius * 9/5) + 32
+
+table = []
 for celsius in range(0, 101, 10):
-    farenheit = (celsius * 9 / 5) + 32
-    print(f"{celsius}\t\t{farenheit:.1f}")
+    table.append([celsius, f"{farenheit:.1f}"])
+print(tabulate(table, headers=["Celsius", "Farenheit"], tablefmt="fancy_grid"))
