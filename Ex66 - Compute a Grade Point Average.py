990a62bb07d79437f88ca75bbd202126a7c92383
Eva Szatmari <szateva@gmail.com>
2023-04-20 22:24:28 +0100

    Ex66 is finished. I've modified the model solution of Ex51, as that is simpler than my version of Ex51.

--- Ex66 - Compute a Grade Point Average.py
+++ Ex66 - Compute a Grade Point Average.py
@@ -8,3 +8,59 @@ error checking. It can assume that each value entered by the user will always be
 
 #TODO - create a few test cases to ensure the program works as expected
 
+A_plus = 4.0
+A = 4.0
+A_minus = 3.7
+B_plus = 3.3
+B = 3.0
+B_minus = 2.7
+C_plus = 2.3
+C = 2.0
+C_minus = 1.7
+D_plus = 1.3
+D = 1.0
+F = 0.0
+
+grade = input("Please enter your letter grade: ")
+average = 0
+sum = 0
+count = 0
+
+while grade != "":
+    grade = grade.upper()
+    if (grade == "A+") or (grade == "A"):
+        sum += A_plus
+        count += 1
+    elif grade == "A-":
+        sum += A_minus
+        count += 1
+    elif grade == "B+":
+        sum += B_plus
+        count += 1
+    elif grade == "B":
+        sum += B
+        count += 1
+    elif grade == "B-":
+        sum += B_minus
+        count += 1
+    elif grade == "C+":
+        sum += C_plus
+        count += 1
+    elif grade == "C":
+        sum += C
+        count += 1
+    elif grade == "C-":
+        sum += C_minus
+        count += 1
+    elif grade == "D+":
+        sum += D_plus
+        count += 1
+    elif grade == "D":
+        sum += D
+        count += 1
+    elif grade == "F":
+        sum += F
+        count += 1
+    grade = input("Please enter your letter grade: (blank to quit) ")
+average = sum / count
+print(f"Your average grade is {average:.2f}.")
