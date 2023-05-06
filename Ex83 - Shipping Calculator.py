1a5e2359530a6d4cad99d36452b64546bd3ba03b
Eva Szatmari <szateva@gmail.com>
2023-05-06 21:58:04 +0100

    Ex83 is solved. Copilot suggested a convoluted solution. There was no point in having an if statement just to single out the shipping charge for 1 item. I wanted the function to return a message if the no of items are less than 1, but the way the program checking the function displays the message is not quite right...

---
+++ Ex83 - Shipping Calculator.py
@@ -0,0 +1,14 @@
+""" An online retailer provides express shipping for many of its items at a rate of $10.95 for the first item,
+and $2.95 for each subsequent item. Write a function that takes the number of items in the order as its only parameter.
+Return the shipping charge for the order as the function’s result. Include a main program that reads the number of items
+purchased from the user and displays the shipping charge. """
+
+def shipping_charge(items):
+    if items < 1:
+        return "Invalid number of items."
+    else:
+        return 10.95 + (items - 1) * 2.95
+
+items = int(input("Please, enter the number of items purchased: "))
+
+print(f"The shipping charge for {items} items is ${shipping_charge(items)}.")
