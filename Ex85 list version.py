bef4f421c19f2c4cf9a8ae6367f7394f71c848ac
Eva Szatmari <szateva@gmail.com>
2023-05-07 22:18:51 +0100

    Ex85 list version is solved. I have to say that copilot has made a simpler code than what I had in mind. I probably would have messed around with the indexing, as I was to put the empty string at the end of the list BUT as the indexing starts with 0 it makes total sense to put the empty string into position 0. Then I modified copilot's suggested solution to return the 0th index in the else branch. It didn't make sense to put the empty string there but then not to use it. However, I have absolutely no idea if this makes any difference in performance.

---
+++ Ex85 list version.py
@@ -0,0 +1,19 @@
+"""Words like first, second and third are referred to as ordinal numbers. In this exercise, you will write a function
+that takes an integer as its only parameter and returns a string containing the appropriate English ordinal number as its
+only result. Your function must handle the integers between 1 and 12 (inclusive). It should return an empty string if a
+value outside of this range is provided as a parameter. Include a main program that demonstrates your function by displaying
+each integer from 1 to 12 and its ordinal number. Your main program should only run when your file has not been imported
+into another program. """
+
+# here I will use lists to store the ordinal numbers
+
+def ordinal_number(number):
+    ordinal_numbers = ["", "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth",
+                       "ninth", "tenth", "eleventh", "twelfth"]
+    if 0 < number < 13:
+        return ordinal_numbers[number]
+    else:
+        return ordinal_numbers[0]
+
+for i in range(1, 13):
+    print(f"{i} is the {ordinal_number(i)} number.")
